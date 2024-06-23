bd49f7574d6435e820c90be0831a0f3db32e56ae
vgiuliani11 <vinicius.giuliani@gmail.com>
2024-06-23 17:39:46 -0300

    Adds book report function

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 def main():
   path = "books/frankenstein.txt"
-  text = get_book_text(path)
-  word_count = get_word_count(text)
-  chars_dict = get_char_count(text)
-  print(f"There are {word_count} words found in the book.")
-  print("Character count:")
-  print(chars_dict)
+  print_book_report(path)
 
 def get_book_text(path):
   with open(path) as f:
@@ -24,5 +19,29 @@ def get_char_count(text):
     else:
       dict[lower_char] += 1
   return dict
+
+def sort_on(dict):
+  return dict["num"]
+
+def get_sorted_dict_list(dict):
+  list = []
+  for key in dict.keys():
+    if key.isalpha():
+      list.append({"char": key, "num": dict[key]})
+  list.sort(reverse=True, key=sort_on)
+  return list
+
+def print_book_report(path):
+  text = get_book_text(path)
+  word_count = get_word_count(text)
+  chars_dict = get_char_count(text)
+  get_sorted_dict_list(chars_dict)
+  char_sorted_list = get_sorted_dict_list(chars_dict)
+  print(f"--- Begin report of {path} ---")
+  print(f"{word_count} words found in the document")
+  print("")
+  for dict in char_sorted_list:
+    print(f"The '{dict["char"]}' character was found {dict["num"]} times")
+  print("--- End report ---")
 
 main()
