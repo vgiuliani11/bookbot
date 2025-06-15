d4bc8b748034d1bd609042b75da9a7b776d9c1ca
Vinicius Giuliani <vinicius.giuliani@gmail.com>
2025-06-15 07:38:17 -0300

    refactor: move functions to stats.py

--- main.py
+++ main.py
@@ -1,47 +1,9 @@
+from stats import print_book_report
+
+
 def main():
-  path = "books/frankenstein.txt"
-  print_book_report(path)
+    path = "books/frankenstein.txt"
+    print_book_report(path)
 
-def get_book_text(path):
-  with open(path) as f:
-    return f.read()
-
-def get_word_count(text):
-  words = text.split()
-  return len(words)
-
-def get_char_count(text):
-  dict = {}
-  for char in text:
-    lower_char = char.lower()
-    if lower_char not in dict:
-      dict[lower_char] = 1
-    else:
-      dict[lower_char] += 1
-  return dict
-
-def sort_on(dict):
-  return dict["num"]
-
-def get_sorted_dict_list(dict):
-  list = []
-  for key in dict.keys():
-    if key.isalpha():
-      list.append({"char": key, "num": dict[key]})
-  list.sort(reverse=True, key=sort_on)
-  return list
-
-def print_book_report(path):
-  text = get_book_text(path)
-  word_count = get_word_count(text)
-  chars_dict = get_char_count(text)
-  get_sorted_dict_list(chars_dict)
-  char_sorted_list = get_sorted_dict_list(chars_dict)
-  print(f"--- Begin report of {path} ---")
-  print(f"{word_count} words found in the document")
-  print("")
-  for dict in char_sorted_list:
-    print(f"The '{dict["char"]}' character was found {dict["num"]} times")
-  print("--- End report ---")
 
 main()
