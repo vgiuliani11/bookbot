6a4656415c71c9b2dbca0e591c73ba25089f66b1
Vinicius Giuliani <vinicius.giuliani@gmail.com>
2025-06-15 08:05:56 -0300

    feat: enable arbitrary book selection using sys.argv to pass the book path as an argument

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import sys
+
 from stats import print_book_report
 
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def main():
-    path = "books/frankenstein.txt"
+    path = sys.argv[1]
     print_book_report(path)
 
 
